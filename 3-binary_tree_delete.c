2f3d306d70326c5b79bde95f7c6cab2c5fa7f7a1
CoeurdArcade <coeurdarcade@gmail.com>
2024-08-06 15:42:01 +0200

    files

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - removes an entire binary tree
+ * @tree: pointer to the root node of the tree to remove
+ *
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
+
