39fe597288d5f28780a366b749f6fbe4c08e88a5
CoeurdArcade <coeurdarcade@gmail.com>
2024-08-08 16:03:54 +0200

    betty style coding spacebar

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -9,12 +9,12 @@
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-        if (tree == NULL || func == NULL)
-                return;
+	if (tree == NULL || func == NULL)
+		return;
 
-        func(tree->n);
-        if (tree->left)
-                binary_tree_preorder(tree->left, func);
-        if (tree->right)
-                binary_tree_preorder(tree->right, func);
+	func(tree->n);
+	if (tree->left)
+		binary_tree_preorder(tree->left, func);
+	if (tree->right)
+		binary_tree_preorder(tree->right, func);
 }
