fb08f946d16e39f5fc1fa6bc5b260b7b75d6d329
CoeurdArcade <coeurdarcade@gmail.com>
2024-08-09 11:12:18 +0200

    update script to be in good with checker

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,33 +1,22 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - goes through a binary tree, in-order traversal
- * @tree: pointer to the root node
- * @func: pointer to a function to call for each node
+ * binary_tree_inorder - print tree in-order traversal
+ * @tree: tree to print
+ * @func: function to print
  *
+ * Return: void
  */
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL || func == NULL)
-		return;
-
-	binary_tree_inorder_recursive(tree, func);
-}
-
-/**
- * binary_tree_inorder_recursive - recursive function for in-order traversal
- * @tree: pointer to the current node
- * @func: pointer to a function to call for each node
- *
- */
-
-void binary_tree_inorder_recursive(const binary_tree_t *tree, void (*func)(int))
-{
-	if (tree == NULL)
-		return;
-
-	binary_tree_inorder_recursive(tree->left, func);
-	func(tree->n);
-	binary_tree_inorder_recursive(tree->right, func);
+	if (tree != NULL)
+	{
+		if (func != NULL)
+		{
+			binary_tree_inorder(tree->left, func);
+			func(tree->n);
+			binary_tree_inorder(tree->right, func);
+		}
+	}
 }
