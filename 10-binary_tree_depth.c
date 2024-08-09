ebe7a9efe015722efccfa780a7c8d1fa50aa30a1
CoeurdArcade <coeurdarcade@gmail.com>
2024-08-09 11:17:17 +0200

    update script to be good with checker

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,31 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - measures the depth of a binary tree
- * @tree: pointer to the root node of the tree
- *
- * Return: depth of tree, 0 if tree is NULL
+ * binary_tree_depth - measures the depth of a node in a tree
+ * @tree: pointer to the node to measure the depth
+ * Return: 0 if tree is null, else the depth of the node in question
  */
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (tree == NULL || tree->parent == NULL)
 		return (0);
 
-	return (depth_helper(tree) - 1);
-}
-
-/**
- * depth_helper - the heavy lifter
- * @tree: tree to measure
- *
- * Return: depth, root inclusive
- */
-
-size_t depth_helper(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-
-	return (1 + depth_helper(tree->parent));
+	return (1 + binary_tree_depth(tree->parent));
 }
