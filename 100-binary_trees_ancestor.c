f814e7ce8e8caa9f86bf62e519926b8d02873638
CoeurdArcade <coeurdarcade@gmail.com>
2024-08-09 17:29:59 +0200

    betty style

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -7,7 +7,8 @@
  * Return: null
  */
 
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
 {
 	const binary_tree_t *temp1, *temp2;
 
